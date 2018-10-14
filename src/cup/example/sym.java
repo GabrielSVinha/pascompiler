c56c437090e420fb109f54d7ebec2d70e54531b3
Gabriel Vinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-10-14 01:03:00 -0300

    Add initial, variables and begin lexemes

--- src/cup/example/sym.java
+++ src/cup/example/sym.java
@@ -9,19 +9,23 @@ package cup.example;
 public interface sym {
   /* terminals */
   public static final int TIMES = 6;
+  public static final int FULLSTOP = 18;
   public static final int PLUS = 3;
   public static final int RPAREN = 8;
   public static final int INT = 15;
   public static final int EQUAL = 17;
   public static final int CHAR = 16;
   public static final int SEMI = 2;
-  public static final int IDENTIFIER = 18;
+  public static final int END = 23;
+  public static final int IDENTIFIER = 19;
   public static final int LPAREN = 7;
   public static final int COLON = 11;
+  public static final int PROGRAM = 21;
+  public static final int BEGIN = 22;
   public static final int COMMA = 12;
   public static final int EOF = 0;
   public static final int NUMBER = 9;
-  public static final int NEWLINE = 19;
+  public static final int NEWLINE = 20;
   public static final int MINUS = 4;
   public static final int CONST = 13;
   public static final int error = 1;
@@ -47,8 +51,12 @@ public interface sym {
   "INT",
   "CHAR",
   "EQUAL",
+  "FULLSTOP",
   "IDENTIFIER",
-  "NEWLINE"
+  "NEWLINE",
+  "PROGRAM",
+  "BEGIN",
+  "END"
   };
 }
 
