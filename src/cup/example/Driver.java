9db1d26437052288d7e6355845503ff262e7f829
Gabriel Vinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-12-02 15:54:13 -0300

    Begin code generation

--- src/cup/example/Driver.java
+++ src/cup/example/Driver.java
@@ -7,6 +7,12 @@ class Driver {
 	public static void main(String[] args) throws Exception {
 		Parser parser = new Parser();
 		parser.parse();
+		if (parser.errors == 0) {
+			System.out.println(parser.code.toString());
+		} else {
+			System.out.println("There were errors in the compilation, check your code.");
+			System.out.println("No code produced.");
+		}
 	}
 	
 }
